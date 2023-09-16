e38e1638b2e24585919a8ab1edbe47b17f76cf80
Nauja <jeremymorosi@hotmail.fr>
2023-09-16 21:27:27 +0200

    improve the api

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import * as MoroboxAIGameSDK from 'moroboxai-game-sdk';
-import * as Moroxel8AISDK from 'moroxel8ai-sdk';
-import * as PIXI from 'pixi.js';
-import { IVM, initVM } from './vm';
-import { PPU, AssetHeader, FontHeader, TileMapHeader } from './ppu';
+import * as MoroboxAIGameSDK from "moroboxai-game-sdk";
+import * as Moroxel8AISDK from "moroxel8ai-sdk";
+import * as PixiMoroxel8AI from "piximoroxel8ai";
+import { IVM, initVM } from "./vm";
+import { PPU, AssetHeader, FontHeader, TileMapHeader } from "./ppu";
+
+export const VERSION = "0.1.0-alpha.8";
 
 const PHYSICS_TIMESTEP = 0.01;
 
@@ -14,7 +16,7 @@ interface ExtendedGameHeader extends MoroboxAIGameSDK.GameHeader {
 }
 
 interface GameScript {
-    language: 'javascript' | 'lua',
+    language: "javascript" | "lua";
     script: string;
 }
 
@@ -24,15 +26,20 @@ interface GameScript {
  * @param {MoroboxAIGameSDK.IGameServer} gameServer - game server for accessing files
  * @returns {Promise} - content of the main script
  */
-function loadMain(header: ExtendedGameHeader, gameServer: MoroboxAIGameSDK.IGameServer): Promise<GameScript> {
+function loadMain(
+    header: ExtendedGameHeader,
+    gameServer: MoroboxAIGameSDK.IGameServer
+): Promise<GameScript> {
     return new Promise<GameScript>((resolve, reject) => {
         if (header.script !== undefined) {
             if (header.language === undefined) {
-                return reject('header is missing language attribute');
+                return reject("header is missing language attribute");
             }
 
-            if (header.language !== 'javascript' && header.language !== 'lua') {
-                return reject(`unknown script language ${header.language} in header`);
+            if (header.language !== "javascript" && header.language !== "lua") {
+                return reject(
+                    `unknown script language ${header.language} in header`
+                );
             }
 
             return resolve({
@@ -42,13 +49,17 @@ function loadMain(header: ExtendedGameHeader, gameServer: MoroboxAIGameSDK.IGame
         }
 
         if (header.main === undefined) {
-            return reject('header is missing main attribute with the path to your main script');
+            return reject(
+                "header is missing main attribute with the path to your main script"
+            );
         }
 
-        return gameServer.get(header.main).then(data => resolve({
-            language: header.main!.endsWith('.js') ? 'javascript' : 'lua',
-            script: data
-        }));
+        return gameServer.get(header.main).then((data) =>
+            resolve({
+                language: header.main!.endsWith(".js") ? "javascript" : "lua",
+                script: data
+            })
+        );
     });
 }
 
@@ -59,7 +70,11 @@ function loadMain(header: ExtendedGameHeader, gameServer: MoroboxAIGameSDK.IGame
  * @param {function} assetLoaded - function called for each loaded asset
  * @returns {Promise} - a promise
  */
-function loadAssets(assets: AssetHeader[] | undefined, gameServer: MoroboxAIGameSDK.IGameServer, assetLoaded: (asset: AssetHeader, res: PIXI.LoaderResource) => void): Promise<void> {
+function loadAssets(
+    assets: AssetHeader[] | undefined,
+    gameServer: MoroboxAIGameSDK.IGameServer,
+    assetLoaded: (asset: AssetHeader, res: PIXI.LoaderResource) => void
+): Promise<void> {
     return new Promise((resolve) => {
         if (assets === undefined || assets.length === 0) {
             // no assets to load
@@ -72,14 +87,14 @@ function loadAssets(assets: AssetHeader[] | undefined, gameServer: MoroboxAIGame
 
         // add each asset to the loader
         const validAssets = new Array<AssetHeader>();
-        assets.forEach(_ => {
+        assets.forEach((_) => {
             if (_.name === undefined) {
-                console.error('skip asset without name');
+                console.error("skip asset without name");
                 return;
             }
 
             if (_.path === undefined) {
-                console.error('skip asset without path');
+                console.error("skip asset without path");
                 return;
             }
 
@@ -90,12 +105,15 @@ function loadAssets(assets: AssetHeader[] | undefined, gameServer: MoroboxAIGame
 
         loader.onComplete.add(() => {
             // dispatch loaded assets
-            validAssets.forEach(_ => {
-                assetLoaded(_, loader.resources[gameServer.href(`assets/${_.path}`)]);
+            validAssets.forEach((_) => {
+                assetLoaded(
+                    _,
+                    loader.resources[gameServer.href(`assets/${_.path}`)]
+                );
             });
 
-            console.log('assets loaded');
-            resolve()
+            console.log("assets loaded");
+            resolve();
         });
         loader.load();
     });
@@ -103,18 +121,18 @@ function loadAssets(assets: AssetHeader[] | undefined, gameServer: MoroboxAIGame
 
 /**
  * Load and initialize the game.
- * @param {MoroboxAIGameSDK.IPlayer} player - player instance 
+ * @param {MoroboxAIGameSDK.IPlayer} player - player instance
  * @param {Function} assetLoaded - function called for each loaded asset
  * @returns {Promise} - content of the main script
  */
-function initGame(player: MoroboxAIGameSDK.IPlayer, assetLoaded: (asset: AssetHeader, res: PIXI.LoaderResource) => void): Promise<GameScript> {
+function initGame(
+    player: MoroboxAIGameSDK.IPlayer,
+    assetLoaded: (asset: AssetHeader, res: PIXI.LoaderResource) => void
+): Promise<GameScript> {
     return new Promise<GameScript>((resolve) => {
         const header = player.header as ExtendedGameHeader;
 
-        return loadMain(
-            header,
-            player.gameServer
-        ).then((data) => {
+        return loadMain(header, player.gameServer).then((data) => {
             return loadAssets(
                 header.assets,
                 player.gameServer,
@@ -155,7 +173,9 @@ class Moroxel8AI implements MoroboxAIGameSDK.IGame, Moroxel8AISDK.IMoroxel8AI {
         this._ppu = new PPU(this._app.renderer);
 
         // init the game and load assets
-        initGame(player, (asset, res) => this._handleAssetLoaded(asset, res)).then((data) => {
+        initGame(player, (asset, res) =>
+            this._handleAssetLoaded(asset, res)
+        ).then((data) => {
             // received the game script
             this._gameScript = data;
             this._initPixiJS();
@@ -225,9 +245,13 @@ class Moroxel8AI implements MoroboxAIGameSDK.IGame, Moroxel8AISDK.IMoroxel8AI {
         this._isPlaying = true;
         this._displayedTickError = false;
 
-        this._vm = initVM(this._gameScript!.language, this._gameScript?.script, this);
+        this._vm = initVM(
+            this._gameScript!.language,
+            this._gameScript?.script,
+            this
+        );
         if (this._vm === undefined) {
-            console.error('failed to create the Lua VM, see errors in console');
+            console.error("failed to create the Lua VM, see errors in console");
             return;
         }
 
@@ -245,7 +269,11 @@ class Moroxel8AI implements MoroboxAIGameSDK.IGame, Moroxel8AISDK.IMoroxel8AI {
     }
 
     stop(): void {
-        this._app.destroy(true, {children: true, texture: true, baseTexture: true});
+        this._app.destroy(true, {
+            children: true,
+            texture: true,
+            baseTexture: true
+        });
     }
 
     resize(): void {
@@ -254,14 +282,25 @@ class Moroxel8AI implements MoroboxAIGameSDK.IGame, Moroxel8AISDK.IMoroxel8AI {
         const realHeight = this._player.height;
 
         this._app.renderer.resize(realWidth, realHeight);
-        this._ppu.sprite.scale.set(realWidth / this.SWIDTH, realHeight / this.SHEIGHT);
+        this._ppu.sprite.scale.set(
+            realWidth / this.SWIDTH,
+            realHeight / this.SHEIGHT
+        );
     }
 
     // IMoroxel8AI interface
-    get SWIDTH(): number { return this._ppu.SWIDTH; }
-    get SHEIGHT(): number { return this._ppu.SHEIGHT; }
-    get TNUM(): number { return this._ppu.TNUM; }
-    get SNUM(): number { return this._ppu.SNUM; }
+    get SWIDTH(): number {
+        return this._ppu.SWIDTH;
+    }
+    get SHEIGHT(): number {
+        return this._ppu.SHEIGHT;
+    }
+    get TNUM(): number {
+        return this._ppu.TNUM;
+    }
+    get SNUM(): number {
+        return this._ppu.SNUM;
+    }
     P1: number = 0;
     P2: number = 1;
     BLEFT: number = 0;
@@ -285,7 +324,10 @@ class Moroxel8AI implements MoroboxAIGameSDK.IGame, Moroxel8AISDK.IMoroxel8AI {
     state(val: any): void;
     state(pid: number, val: any): void;
     state(pid: any | number, val?: any): void {
-        this._player.sendState(val === undefined ? pid : val, val === undefined ? undefined : pid);
+        this._player.sendState(
+            val === undefined ? pid : val,
+            val === undefined ? undefined : pid
+        );
     }
 
     btn(bid: number): boolean;
@@ -394,6 +436,40 @@ class Moroxel8AI implements MoroboxAIGameSDK.IGame, Moroxel8AISDK.IMoroxel8AI {
     }
 }
 
+/**
+ * Act as a game for PixiMoroxel8AI.
+ */
+class Game implements MoroboxAIGameSDK.IGame {
+    help(): string {
+        throw new Error("Method not implemented.");
+    }
+    play(): void {
+        throw new Error("Method not implemented.");
+    }
+    pause(): void {
+        throw new Error("Method not implemented.");
+    }
+    stop(): void {
+        throw new Error("Method not implemented.");
+    }
+    resize(): void {
+        throw new Error("Method not implemented.");
+    }
+    saveState(): object {
+        throw new Error("Method not implemented.");
+    }
+    loadState(state: object): void {
+        throw new Error("Method not implemented.");
+    }
+    getStateForAgent(): object {
+        throw new Error("Method not implemented.");
+    }
+    ticker?: ((delta: number) => void) | undefined;
+    tick(inputs: MoroboxAIGameSDK.IInputs[], delta: number): void {
+        throw new Error("Method not implemented.");
+    }
+}
+
 export const boot: MoroboxAIGameSDK.IBoot = (player) => {
-    return new Moroxel8AI(player);
+    return PixiMoroxel8AI.boot(player);
 };
